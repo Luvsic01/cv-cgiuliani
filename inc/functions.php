2fdf754fb552d76e458e816adddf8818ef2aef35
Cyril Giuliani <giuliani.cyril@gmail.com>
2017-11-10 17:00:52 +0100

    Création login
    Formulaire de connection 
    function:
    connection 
    vérification pwd
    vérification user

--- inc/functions.php
+++ inc/functions.php
@@ -49,3 +49,45 @@ function sendEmailContact($from, $name, $subject, $htmlContent, $texContent=''){
         return false;
     }
 }
+
+// Check si l'utilisateur existe
+function usrEmailExist($email){
+    global $pdo;
+    $requestSql = "SELECT usr_id FROM users WHERE usr_email = :email";
+    $pdoStatement = $pdo->prepare($requestSql);
+    $pdoStatement->bindValue('email', $email, PDO::PARAM_STR);
+    if ($pdoStatement->execute() === false){
+        print_r($pdoStatement->errorInfo());
+        exit();
+    }
+    if ($pdoStatement->rowCount() > 0){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+//Fonction de vérification du password
+function checkPassword($email, $password){
+    global $pdo;
+    $requestHash = "SELECT usr_password FROM users WHERE usr_email = :email";
+    $pdoStatementCheckPwd = $pdo->prepare($requestHash);
+    $pdoStatementCheckPwd->bindValue(":email", $email, PDO::PARAM_STR);
+    if ($pdoStatementCheckPwd->execute() === false){
+        print_r($pdoStatementCheckPwd->errorInfo());
+        exit();
+    }
+    $result = $pdoStatementCheckPwd->fetch(PDO::FETCH_ASSOC);
+    $hash = $result['usr_password'];
+    if (password_verify($password, $hash)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Fonction de création de la session
+function createSession($email){
+    $_SESSION['email'] = $email;
+    $_SESSION['ip'] = $_SERVER["REMOTE_ADDR"];
+}
