2e86b061f22302edbcca4ffbfd81709135c530d6
Cyril Giuliani <giuliani.cyril@gmail.com>
2017-11-10 15:15:33 +0100

    Validation des données
    Validation des données du formulaire
    trim + striptag + longeur
    Remplissage donnée saisie afin d'éviter la re-saisie

--- public/index.php
+++ public/index.php
@@ -1,5 +1,48 @@
 <?php
 
+// Variables du formulaire
+$infoForm = '';
+$formOk = true;
+
+// Formulaire contact submit
+if (!empty($_POST)){
+    // On récupère les donnée envoyer
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $subject = $_POST['subject'] ?? '';
+    $msg = $_POST['msg'] ?? '';
+
+    // Nettoyage des variables
+    $name = trim(strip_tags($name));
+    $email = trim(strip_tags($email));
+    $subject = trim(strip_tags($subject));
+    $msg = trim(strip_tags($msg));
+
+    // Validation du nom
+    if ( empty($name) ){
+        $infoForm .= "Veuillez renseigner un prenom<br>";
+        $formOk = false;
+    } elseif (strlen($name) < 2){
+        $infoForm .= "Veuillez renseigner un prenom d'au moins 2 caractères<br>";
+        $formOk = false;
+    }
+    // validation email
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $infoForm .='Veuillez renseigner un Email correct<br>';
+        $formOk = false;
+    }
+    // Validation du sujet
+    if (empty($subject)){
+        $infoForm .= "Veuillez renseigner sujet<br>";
+        $formOk = false;
+    }
+    // Validation msg
+    if (empty($msg)){
+        $infoForm .= "Veuillez renseigner message<br>";
+        $formOk = false;
+    }
+    
+}
 
 // Title page
 $title = "CV Cyril Giuliani";
--- view/contact.php
+++ view/contact.php
@@ -1,13 +1,14 @@
 <section id="contact">
     <h2>Contact</h2>
     <form method="post" action="">
-        <label for="name"></label>
-        <input id="name" name="name" type="text" placeholder="* Votre nom">
-        <label for="email"></label>
-        <input id="email" name="email" type="text" placeholder="* Votre email ">
-        <label for="subject"></label>
-        <input id="subject" name="subject" type="text" placeholder="* Sujet de votre demande">
-        <label for="msg"></label>
-        <textarea id="msg" name="msg" placeholder="* Votre message"></textarea>
+        <label for="name">Nom :</label>
+        <input id="name" name="name" type="text" placeholder="* Votre nom" value="<?= $name ?? '' ?>">
+        <label for="email">Email :</label>
+        <input id="email" name="email" type="email" placeholder="* Votre email " value="<?= $email ?? '' ?>">
+        <label for="subject">Sujet :</label>
+        <input id="subject" name="subject" type="text" placeholder="* Sujet de votre demande" value="<?= $subject ?? '' ?>">
+        <label for="msg">Message :</label>
+        <textarea id="msg" name="msg" placeholder="* Votre message"><?= $msg ?? '' ?></textarea>
+        <input type="submit" value="Envoyer">
     </form>
 </section>
